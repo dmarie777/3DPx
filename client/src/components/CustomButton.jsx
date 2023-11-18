44bda8718a29e93efff8d134db71d06aa0766040
dmarie777 <deimarie777@gmail.com>
2023-11-18 14:05:20 -0500

    Added CustomButton component into Home.jsx file

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CustomButton = ({ title, type, customStyles, handleClick }) => {
+  return (
+    <button
+    
+    >
+      {title}
+    </button>
+  )
+}
+
+export default CustomButton
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { motion, AnimatePresence } from 'framer-motion'
 import { useSnapshot } from 'valtio'
 import state from '../store/index'
+import { CustomButton } from '../components'
 
 import {
     headContainerAnimation,
@@ -22,7 +23,6 @@ const Home = () => {
               alt='logo'
               className='w-8 h-8 object-contain'
             />
-
           </motion.header>
 
           <motion.div className='home-content' {...headContainerAnimation}>
@@ -30,7 +30,21 @@ const Home = () => {
               <h1 className='head-text'>
                 LET'S <br className='xl:block hidden' /> DO IT
               </h1>
-
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className='flex flex-col gap-5'
+            >
+              <p className='max-w-md font-normal text-gray-600 text-base' >
+                Embarking on my initial exploration into the world of 3D and animation. Where endless possibilities unfold at every turn. A journey filled with simplicity and discovery.
+              </p>
+              
+              <CustomButton
+                type = "filled"
+                title = "Customize It"
+                handleClick = {() => state.intro = false}
+                customStyles = "w-fit px-4 py-2.5 font-bold text-sm"
+              />
             </motion.div>
 
           </motion.div>
