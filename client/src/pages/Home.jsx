4564a5c02cd5b206d45ed90bf2f3cd2f8fec8acf
dmarie777 <deimarie777@gmail.com>
2023-11-18 11:48:52 -0500

    Added proxy-state inside the index.js file, Added logo and h1 in the Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+import state from '../store/index'
+
+import {
+    headContainerAnimation,
+    headContentAnimation,
+    headTextAnimation,
+    slideAnimation
+} from '../config/motion'
 
 const Home = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>Home</div>
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation('left')}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src = './threejs.png'
+              alt='logo'
+              className='w-8 h-8 object-contain'
+            />
+
+          </motion.header>
+
+          <motion.div className='home-content' {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className='head-text'>
+                LET'S <br className='xl:block hidden' /> DO IT
+              </h1>
+
+            </motion.div>
+
+          </motion.div>
+
+
+        </motion.section>
+      )}
+    </AnimatePresence>
   )
 }
 
