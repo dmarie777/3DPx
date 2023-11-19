70fab9fe5a9bd6661503b76122073c1f24547d6d
dmarie777 <deimarie777@gmail.com>
2023-11-18 19:11:41 -0500

    Imported the components into the index.js file and the Customizer.jsx file

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -8,8 +8,11 @@ import { download } from '../assets'
 import { downloadCanvasToImage, reader } from '../config/helpers'
 import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants'
 import { fadeAnimation, slideAnimation }  from '../config/motion'
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components'
 
 const Customizer = () => {
+  const snap = useSnapshot(state)
+
   return (
     <div>Customizer</div>
   )
